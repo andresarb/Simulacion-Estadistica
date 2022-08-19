4b46081a74d2b547d534f4835ae8cc5cfd9a4174
andresarb <110797338+andresarb@users.noreply.github.com>
2022-08-19 17:21:31 -0500

    Update Transformada Inversa para Discreta Uniforme.R

--- Generación de Variables Aleatorias/Transformada Inversa para Discreta Uniforme.R
+++ Generación de Variables Aleatorias/Transformada Inversa para Discreta Uniforme.R
@@ -1,21 +1,14 @@
 # FUNCIÓN PARA GENERAR UN CONJUNTO DE DATOS CON DISTRIBUCIÓN UNIFORME DISCRETA
 # Método de Transformación Inversa
 
-# k: Número elementos en la variable aleatoria
-# u: Número de datos que desea generar
+# m: Número elementos en la variable aleatoria
+# n: Número de datos que desea generar
 
-unifdis <- function(u,k) {
-  V<-runif(u)
-  X<-numeric(u)
-  for(i in 1:u){
-    j=1
-    while((X[i])==0){
-      if(V[i]<(j*(1/k))){
-        X[i]<-j
-      }else {
-        j<-j+1
-      }
-    }
+unifdis<-function(n,m){
+  V<-runif(n)
+  X<-numeric(n)
+  for (i in 1:n){
+    X[i]=floor(m*V[i])+1
   }
   return(X)
 }
